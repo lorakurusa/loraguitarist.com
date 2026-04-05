a5153c4b788c92253ad78a0f018340944e5fcbbc
lorakurusa <lora.kurusa@gmail.com>
2026-04-05 12:40:25 +1200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-/**Nav java**/
-const hamburger = document.getElementById("hamburger");
-const mobilePanel = document.getElementById("mobilePanel");
-
-hamburger.addEventListener("click", () => {
-  mobilePanel.classList.toggle("show");
-});
